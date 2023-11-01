d000384090384bf65923b45b94a4058bb9653292
girishsatyam <girishsatyam@gmail.com>
2023-11-01 13:09:17 +0530

    added three new models def's

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print("adding all Ml Related code here")
+
+def model1():
+    print("Adding code for ML Model1")
+
+def model2():
+    print("Adding code for ML Model2")
+
+def model3():
+    print("Adding code for ML Model3")
